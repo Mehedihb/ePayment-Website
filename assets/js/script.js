315137b4af52f7ab88ab01f2b74a4472fbca885c
Mehedi Hassan <mehedi.all.info@gmail.com>
2023-07-25 00:06:04 +0600

    updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,9 @@
-// Function to handle the 'load' event
-const handleWindowLoad = () => {
-    document.body.classList.add('loaded');
-  };
-  
-  // Add event listener for the 'load' event
-  window.addEventListener('load', handleWindowLoad);
-  
-
 /********************
 header
 ********************/
 // Function to handle scroll event and toggle class 'fixed' for .header-1
 const handleScrollHeader1 = () => {
-    const header1 = document.querySelector('.header-1');
+    const header1 = document.querySelector('.header');
     if (window.scrollY > 50) {
       header1.classList.add('fixed');
     } else {
@@ -22,34 +13,6 @@ const handleScrollHeader1 = () => {
   
   // Add event listeners for scroll events
   window.addEventListener('scroll', handleScrollHeader1);
-  
-
-
-
-/********************
-accordion
-********************/
-// Function to handle 'hide.bs.collapse' event
-const handleHideCollapse = (e) => {
-    const parentId = e.target.closest('.card').id;
-    document.querySelectorAll(`#${parentId} > .card-header > h5`).forEach(h5 => {
-      h5.classList.add('hidden');
-    });
-  };
-  
-  // Function to handle 'show.bs.collapse' event
-  const handleShowCollapse = (e) => {
-    const parentId = e.target.closest('.card').id;
-    document.querySelectorAll(`#${parentId} > .card-header > h5`).forEach(h5 => {
-      h5.classList.remove('hidden');
-    });
-  };
-  
-  // Add event listeners for 'hide.bs.collapse' and 'show.bs.collapse' events
-  document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('hide.bs.collapse', handleHideCollapse);
-    card.addEventListener('show.bs.collapse', handleShowCollapse);
-  });
   
 
 

135960f451c1b1b39bbcbd736d5efdda50b60000
Mehedi Hassan <mehedi.all.info@gmail.com>
2023-07-26 00:53:45 +0600

    New Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ header
 ********************/
 // Function to handle scroll event and toggle class 'fixed' for .header-1
 const handleScrollHeader1 = () => {
-    const header1 = document.querySelector('.header');
+    const header1 = document.querySelector('.headerScroll');
     if (window.scrollY > 50) {
       header1.classList.add('fixed');
     } else {
@@ -19,6 +19,19 @@ const handleScrollHeader1 = () => {
 
 
 
+      // Start Category Sidebar Area
+      var categorySidebar = document.querySelector('.category-sidebar');
+      var categoryToggle = document.querySelectorAll('.category-menu , .category-menu-mobile');
+      var categoryClose = document.querySelector('.categoryClose-btn')
+      categoryToggle.forEach( (node) => {
+        node.addEventListener('click', () => {
+          categorySidebar.classList.toggle('show-hide');
+        })
+      })
+      categoryClose.addEventListener("click", ()=>{
+        categorySidebar.classList.remove('show-hide');
+      })
+    // End Category Sidebar Area
 
 
 

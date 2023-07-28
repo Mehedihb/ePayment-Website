a01bea66f0c532c56d4cfe2c6144ebe38e75cce3
Mehedi Hassan <mehedi.all.info@gmail.com>
2023-07-28 15:46:50 +0600

    Temporary Wizard uploaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,90 @@
 
+
+(function () {
+  var wizard = {
+    formstate: 0,
+    inactiveSections: [1, 2],
+    setInactiveSections: function () {
+      if (this.formstate === 0) {
+        this.inactiveSections = [1, 2];
+      } else if (this.formstate === 1) {
+        this.inactiveSections = [0, 2];
+      } else {
+        this.inactiveSections = [0, 1];
+      }
+    },
+    formsections: ['#aboutcont', '#addrescont', '#verifycont'],
+    circlesections: ['.aboutblock .circulo', '.addressblock .circulo', '.verifyblock .circulo'],
+    prevbtn: 'prevbtn',
+    nextbtn: 'nextbtn',
+
+    initiateForm: function () {
+      var currsection = document.querySelector(this.formsections[this.formstate]);
+      var inactiveSecta = document.querySelector(this.formsections[this.inactiveSections[0]]);
+      var inactiveSectb = document.querySelector(this.formsections[this.inactiveSections[1]]);
+      var currcircle = document.querySelector(this.circlesections[this.formstate]);
+
+      var progressbar = document.querySelector('#progresswizard');
+      var currsectionprogress = currsection.getAttribute('data-progress');
+
+      progressbar.style.width = currsectionprogress;
+      currsection.style.display = "block";
+      inactiveSecta.style.display = "none";
+      inactiveSectb.style.display = "none";
+
+      if (this.formstate === 0) {
+        document.getElementById(this.prevbtn).style.display = "none";
+      } else {
+        document.getElementById(this.prevbtn).style.display = "inline-block";
+      }
+
+      currcircle.classList.add("activecirculo");
+      this.setInactiveCircles();
+
+      if (this.formstate === 2) {
+        document.querySelector("#nextbtn").style.display = "none";
+        document.querySelector("#submitForm").style.display = "inline-block";
+      } else {
+        document.querySelector("#nextbtn").style.display = "inline-block";
+        document.querySelector("#submitForm").style.display = "none";
+      }
+    },
+
+    nextSection: function () {
+      if (this.formstate < 2) {
+        this.formstate++;
+        this.setInactiveSections();
+        wizard.initiateForm();
+      }
+    },
+
+    prevSection: function () {
+      if (this.formstate > 0) {
+        this.formstate--;
+        this.setInactiveSections();
+        wizard.initiateForm();
+      }
+    },
+  };
+
+  wizard.initiateForm();
+  document.getElementById(wizard.nextbtn).addEventListener("click", function () { wizard.nextSection(); });
+  document.getElementById(wizard.prevbtn).addEventListener("click", function () { wizard.prevSection(); });
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // JavaScript to handle the custom select behavior
 document.addEventListener("DOMContentLoaded", function () {
   var customSelect = document.querySelector(".custom-select");
@@ -17,27 +103,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   var customSelect = document.querySelector(".custom-select");
-//   var selectedOption = customSelect.querySelector(".select-selected");
-//   var dropdownItems = customSelect.querySelector(".select-items").children;
-
-//   selectedOption.addEventListener("click", function () {
-//     var selectItems = this.nextElementSibling;
-//     selectItems.style.display = selectItems.style.display === "block" ? "none" : "block";
-//     selectedOption.classList.add("clickedColor"); // Add the .clickedColor class to selectedOption
-//   });
-
-//   for (var i = 0; i < dropdownItems.length; i++) {
-//     dropdownItems[i].addEventListener("click", function () {
-//       selectedOption.textContent = this.textContent;
-//       selectedOption.click();
-//     });
-//   }
-// });
-
-
 
 /********************
 header
